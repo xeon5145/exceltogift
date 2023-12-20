1c44eb60bd3bcddf8c148a0e5e925389409ed591
Abhishek Kagra <abhishekkagraaaddoo.net>
2023-12-20 12:33:12 +0530

    data displayed on page

--- api/qbFormater.php
+++ api/qbFormater.php
@@ -20,14 +20,79 @@ if (isset($_FILES['questionBank']) && $_FILES['questionBank']['error'] === UPLOA
     echo '<h2>CSV Data:</h2>';
 
     // getting job role name
-    echo $jobRoleName = $_POST['qbName']."<br>";
-    echo $qblanguage = $_POST['langauge']."<br>";
+    $jobRoleName = $_POST['qbName']; //Name of question bank
+    $qblanguage = $_POST['langauge']; //Language of question bank
     // getting job role name
 
-    for($i = 1 ; $i < count($data);$i++)
-    {
-        var_dump($data[$i]);
+    // switch category 1
+    echo $catswitch1 = '$CATEGORY : top/Default for Ambuja/' . $jobRoleName;
+    echo "<br>";
+    // switch category 1
 
+    // switch category 2
+    echo "<br>";
+    echo $catswitch2 = $catswitch1 . '/Language : ' . $qblanguage;
+    echo "<br>";
+
+    // switch category 2
+
+    for ($i = 1; $i < count($data); $i++) {
+
+        // switch category 3
+        echo "<br>";
+        echo $catswitch3 = $catswitch2 . '/' . $data[$i][1] . ' - ' . $data[$i][2];
+        echo "<br>";
+
+        // switch category 3
+
+        // writing data
+        echo "<br>";
+        echo $questionopen = '::' . $data[$i][3] . ' {';
+        echo "<br>";
+
+        // options and answers
+        $answer = $data[$i][8];
+        switch ($answer) {
+            case 'A':
+                $option1 = '=' . $data[$i][4];
+                $option2 = '~' . $data[$i][5];
+                $option3 = '~' . $data[$i][6];
+                $option4 = '~' . $data[$i][7];
+                break;
+
+            case 'B':
+                $option1 = '~' . $data[$i][4];
+                $option2 = '=' . $data[$i][5];
+                $option3 = '~' . $data[$i][6];
+                $option4 = '~' . $data[$i][7];
+                break;
+
+            case 'C':
+                $option1 = '~' . $data[$i][4];
+                $option2 = '~' . $data[$i][5];
+                $option3 = '=' . $data[$i][6];
+                $option4 = '~' . $data[$i][7];
+                break;
+
+            case 'D':
+                $option1 = '~' . $data[$i][4];
+                $option2 = '~' . $data[$i][5];
+                $option3 = '~' . $data[$i][6];
+                $option4 = '=' . $data[$i][7];
+                break;
+        }
+        // options and answers
+        echo $option1;
+        echo '<br>';
+        echo $option2;
+        echo '<br>';
+        echo $option3;
+        echo '<br>';
+        echo $option4;
+        echo '<br>';
+        echo $questionclose = '}';
+        echo "<br>";
+        // writing data
     }
 }
 ?>
